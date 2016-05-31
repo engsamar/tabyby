56fd31bd212bcd931ae6667b9770cdbd65984376
abeer <abeer.elhout@gmail.com>
2016-05-31 14:16:00 +0200

    delete and update 3

---
+++ database/migrations/2016_05_31_120931_create_complains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComplainsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complains', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('complain');
+            $table->string('h_of_complain');
+            $table->integer('reservation_id')->unsigned();
+            $table->foreign('reservation_id')->references('id')->on('reservations')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('complains');
+    }
+
+}

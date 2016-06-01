514a4c9a8c1352e69796e63871eebec4b0a7520c
engsamar <eng.samar.2014@gmail.com>
2016-06-01 16:47:46 +0200

    add authController,css and js file

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'email', 'password','address', 'telephone', 'mobile', 'birthdate',
     ];
 
     /**
